6a64f76dd1187025ccb8290b6b48910c92513a2a
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-03-23 20:59:25 +0000

    I'm not happy with how you can't switch wood types of drawers
    (this will also be implemented for soph storage)
    
    Signed-off-by: Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>

--- kubejs/server_scripts/mods/functionalstorage.js
+++ kubejs/server_scripts/mods/functionalstorage.js
@@ -1,29 +1,31 @@
 if(Platform.isLoaded("functionalstorage")) {
     ServerEvents.recipes(event => {
-        zincMachine(event, Item.of("functionalstorage:storage_controller", 1), MC("diamond"))
-        zincMachine(event, Item.of("functionalstorage:controller_extension", 1), MC("gold_ingot"))
-        zincMachine(event, Item.of("functionalstorage:simple_compacting_drawer", 1), CR("mechanical_piston"))
-        zincMachine(event, Item.of("functionalstorage:compacting_drawer", 1), CR("sticky_mechanical_piston"))
-        enderiumMachine(event, Item.of("functionalstorage:fluid_1", 4))
-        enderiumMachine(event, Item.of("functionalstorage:fluid_2", 4))
-        enderiumMachine(event, Item.of("functionalstorage:fluid_4", 4))
-        enderiumMachine(event, Item.of("functionalstorage:ender_drawer", 1))
+        zincMachine(event, Item.of(FS("storage_controller"), 1), MC("diamond"))
+        zincMachine(event, Item.of(FS("controller_extension"), 1), MC("gold_ingot"))
+        zincMachine(event, Item.of(FS("simple_compacting_drawer"), 1), CR("mechanical_piston"))
+        zincMachine(event, Item.of(FS("compacting_drawer"), 1), CR("sticky_mechanical_piston"))
+        enderiumMachine(event, Item.of(FS("fluid_1"), 4))
+        enderiumMachine(event, Item.of(FS("fluid_2"), 4))
+        enderiumMachine(event, Item.of(FS("fluid_4"), 4))
+        enderiumMachine(event, Item.of(FS("ender_drawer"), 1))
 
-        event.remove({id:"functionalstorage:oak_drawer_alternate_x1"})
+        event.remove({id:FS("oak_drawer_alternate_x1")})
         // framed drawers
-        event.remove({id:"functionalstorage:compacting_framed_drawer"})
-        event.remove({id:"functionalstorage:framed_storage_controller"})
-        event.remove({id:"functionalstorage:framed_controller_extension"})
-        event.remove({id:"functionalstorage:framed_simple_compacting_drawer"})
-        let pattern = ["III","IDI","III"]
-        event.shaped(Item.of("functionalstorage:compacting_framed_drawer", 1), pattern,
-            {I:"#forge:nuggets/iron", D:"functionalstorage:compacting_drawer"})
-        event.shaped(Item.of("functionalstorage:framed_storage_controller", 1), pattern,
-            {I:"#forge:nuggets/iron", D:"functionalstorage:storage_controller"})
-        event.shaped(Item.of("functionalstorage:framed_controller_extension", 1), pattern,
-            {I:"#forge:nuggets/iron", D:"functionalstorage:controller_extension"})
-        event.shaped(Item.of("functionalstorage:framed_simple_compacting_drawer", 1), pattern,
-            {I:"#forge:nuggets/iron", D:"functionalstorage:simple_compacting_drawer"})
+        event.remove({id:FS("compacting_framed_drawer")})
+        event.remove({id:FS("framed_storage_controller")})
+        event.remove({id:FS("framed_controller_extension")})
+        event.remove({id:FS("framed_simple_compacting_drawer")})
+        
+        donutCraft(event, FS("compacting_framed_drawer"), FS("compacting_drawer"), "#forge:nuggets/iron")
+        donutCraft(event, FS("framed_storage_controller"), FS("storage_controller"), "#forge:nuggets/iron")
+        donutCraft(event, FS("framed_controller_extension"), FS("controller_extension"), "#forge:nuggets/iron")
+        donutCraft(event, FS("framed_simple_compacting_drawer"), FS("simple_compacting_drawer"), "#forge:nuggets/iron")
+
+        let fsWoodTypes = [MC("oak"), MC("spruce"), MC("birch"), MC("jungle"), MC("acacia"), MC("dark_oak"), MC("mangrove"), MC("cherry"), MC("crimson"), MC("warped")]
+        
+        for (let i=1; i==4; i++) {
+
+        }
     })
 
     ServerEvents.tags("block", event => {
